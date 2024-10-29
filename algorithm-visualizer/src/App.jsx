6d7c7d838cd0e330c3051c8cba9614945abb544c
HectorMendezCIP <hector.mendez.fernandez@est.una.ac.cr>
2024-10-29 00:23:54 -0600

    Update App.jsx display

--- algorithm-visualizer/src/App.jsx
+++ algorithm-visualizer/src/App.jsx
@@ -1,16 +1,33 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+// import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 // import BubbleSort from './algorithms/BubbleSort'
 import BubbleSortVisualizer from './algorithms/BubbleSortVisualizer'
+import SelectionSortVisualizer from './algorithms/SelectionSortVisualizer'
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<BubbleSortVisualizer />} />
-       {/* More routes for other algorithms */}
-      </Routes>
-    </Router>
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Algorithm Visualizer</h1>
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 1fr", // Tres columnas
+        gridTemplateRows: "auto auto auto", // Tres filas (una por algoritmo)
+        gap: "20px",
+        marginTop: "20px"
+      }}>
+        {/* Bubble Sort Visualizer */}
+        <div style={{ gridColumn: "1 / 2", gridRow: "1", border: "1px solid #ddd", borderRadius: "8px", padding: "20px" }}>
+          <h2>Bubble Sort</h2>
+          <BubbleSortVisualizer />
+        </div>
+
+        {/* Selection Sort Visualizer */}
+        <div style={{ gridColumn: "2 / 3", gridRow: "1", border: "1px solid #ddd", borderRadius: "8px", padding: "20px" }}>
+          <h2>Selection Sort</h2>
+          <SelectionSortVisualizer />
+        </div>
+      </div>
+    </div>
   )
 }
 
